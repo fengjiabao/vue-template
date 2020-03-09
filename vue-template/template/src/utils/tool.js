b4f1d2e3fade434cd26bd03b11606f00e87353f9
luokaibin <15686123825@163.com>
2020-03-09 21:28:48 +0800

    fix

--- vue-template/template/src/utils/tool.js
+++
@@ -1,7 +0,0 @@
-// 验证空值
-export const validateNullFn = (rule, value, callback) => {
-  const { field } = rule;
-  value = value.trim();
-  value ? callback() : callback(new Error(`${field}不能为空`));
-  return value ? true : false;
-};
